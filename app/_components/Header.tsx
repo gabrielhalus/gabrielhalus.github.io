665f92e5e719012390682010c75f5e5ce148bb4c
Gabriel Halus <gabriel.halus@businessdecision.com>
2024-07-03 14:42:09 +0200

    Add a link to root on the header

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,6 +2,7 @@
 import { useTranslation } from 'react-i18next';
 import { Section } from './Section';
 import { LanguagesDropdown } from './ui/LanguagesDropdown';
+import Link from 'next/link';
 
 export const Header = () => {
   const { t } = useTranslation('home');
@@ -9,7 +10,9 @@ export const Header = () => {
   return (
     <header className='sticky top-0 py-4 z-10 bg-background'>
       <Section className='flex items-baseline'>
-        <h1 className='text-lg font-bold text-foreground'>{t('header.title')}</h1>
+        <Link href='/'>
+          <h1 className='text-lg font-bold text-foreground'>{t('header.title')}</h1>
+        </Link>
         <div className='flex-1' />
         <ul className='flex items-center gap-2'>
           <LanguagesDropdown />
