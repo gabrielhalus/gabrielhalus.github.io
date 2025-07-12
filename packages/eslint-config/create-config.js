b0d0d2b1b7d74ab115794ae50dce1d949ed83c93
Gabriel Halus <gabrielhalus@gmail.com>
2025-07-12 11:33:38 +0200

    feat(eslint-config): add next support

--- packages/eslint-config/create-config.js
+++ packages/eslint-config/create-config.js
@@ -1,19 +1,30 @@
 import antfu from "@antfu/eslint-config";
 
-export default function createConfig(options, ...userConfigs) {
-  return antfu(
-    {
-      type: "app",
-      typescript: true,
-      formatters: true,
-      stylistic: {
-        indent: 2,
-        semi: true,
-        quotes: "double",
-        braceStyle: "1tbs",
-      },
-      ...options,
+export default async function createConfig(options, ...userConfigs) {
+  const config = {
+    type: "app",
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: "double",
+      braceStyle: "1tbs",
     },
+    ...options,
+  };
+
+  // If Next.js is enabled, add the plugin
+  if (options.next) {
+    const nextPlugin = await import("@next/eslint-plugin-next");
+    config.plugins = {
+      ...config.plugins,
+      "@next/next": nextPlugin.default,
+    };
+  }
+
+  return antfu(
+    config,
     {
       rules: {
         "style/brace-style": ["error", "1tbs", { allowSingleLine: true }],
